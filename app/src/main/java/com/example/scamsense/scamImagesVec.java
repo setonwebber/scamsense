b95c0df62f049740f2e56342d57020984219fc61
Seton Webber <setonwebber@gmail.com>
2024-08-30 12:29:40 +0800

    added comments to the code

--- app/src/main/java/com/example/scamsense/scamImagesVec.java
+++ app/src/main/java/com/example/scamsense/scamImagesVec.java
@@ -26,40 +26,50 @@ public class scamImagesVec {
     public void loadVector(AssetManager assetManager) {
         Log.d("PRINTCONSOLE", "loadVector variables loaded.");
 
+        // loop through every subfolder in /scamimages/ (currently hardcoded to iteration imageCount amount of times, we can try change to actually check how many folders there is and go from there).
         for (int i = 1; i <= imageCount; i++) {
+            // make a string of the directory path for the subfolder we're currently on
             String directoryPath = "scamImages/" + i;
             Log.d("PRINTCONSOLE", "Attempting to load assets from: " + directoryPath);
 
+            // try catch just in case lol :3
             try {
+                // set variables for the scamImage object, isScam and subtext are placehoders for now, where the fileLocations will lead to the files (make sure the naming stays consistent
                 boolean isScam = false;
                 String fileLocation = directoryPath + "/scam.png";
                 String overlayFileLocation = directoryPath + "/overlay.png";
                 String subtext = "";
 
-                // Open the text file
+                // open the text file
                 InputStream inputStream = assetManager.open(directoryPath + "/text.txt");
                 Log.d("PRINTCONSOLE", "input stream opened.");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
                 Log.d("PRINTCONSOLE", "Text file opened.");
 
-                // Read the first line to determine if it's a scam
+                // read the first line to determine if it's a scam
                 String firstLine = reader.readLine();
-                if (firstLine != null && firstLine.length() >= 13 && firstLine.substring(9, 13).equals("true")) {
-                    isScam = true;
-                }
 
-                // Read the rest of the file and append to subtext
+                // compare the last 4 characters of the firstline of the text file against true.
+                if (firstLine != null && firstLine.length() >= 13 && firstLine.substring(9, 13).equals("true")) {
+                    // change isScam to true.
+                    isScam = true;
+                } // else isScam is already set to false so we dont need to do anything here.
+
+                // read the rest of the file and append to subtext
                 String line;
                 StringBuilder subtextBuilder = new StringBuilder();
+                // loop through, while lines exist (stops when we reach the end).
                 while ((line = reader.readLine()) != null) {
+                    // append each line into the string subtext, each time with a line break.
                     subtextBuilder.append(line).append("\n");
                 }
+                // turn stringbuilder into a string... (it was like a list thing before idk its weird but it works)
                 subtext = subtextBuilder.toString();
 
-                // Close the reader
+                // close the reader
                 reader.close();
 
-                // Create a ScamImage object and add it to the list
+                // create a ScamImage object and add it to the list
                 scamImage currentScam = new scamImage(isScam, fileLocation, overlayFileLocation, subtext);
                 scamImages.add(currentScam);
                 Log.d("PRINTCONSOLE", "ScamImage loaded and added to list.");
